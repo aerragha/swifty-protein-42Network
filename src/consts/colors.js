a0c5df19c9919f80e1e7be1994ae5af28537e513
Erraghay ayoub <erraghayayoub@gmail.com>
2022-05-18 18:31:39 +0100

    ✅ Finish Login screen Ui

--- src/consts/colors.js
+++ src/consts/colors.js
@@ -3,6 +3,7 @@ const COLORS = {
     black: '#000',
     red: '#AC0000',
     light: '#F1F1F1',
+    grey: '#666666'
 }
 
 export default COLORS   
--- src/styles/Ligends.styles.js
+++ src/styles/Ligends.styles.js
@@ -1,10 +1,52 @@
 import { StyleSheet } from "react-native";
+import COLORS from "../consts/colors";
 
 export default StyleSheet.create({
-    test: {
-        flex: 1,
-        // width: '100%',
-        // height: '100%',
-        backgroundColor: 'white',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  textContainer: {
+    flex: 0.2,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  text: {
+    color: COLORS.grey,
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 25,
+    textAlign: "center",
+  },
+  ImageContainer: {
+    flex: 0.6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    flex: 1,
+    width: "40%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  btnContainer: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  goButton: {
+    backgroundColor: COLORS.red,
+    width: "90%",
+    height: "30%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  btnText: {
+    color: COLORS.white,
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 19,
+  },
 });
